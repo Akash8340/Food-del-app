f75de11fc0138f8941d48677b4062ad2c7b20dca
Akash8340 <146644610+Akash8340@users.noreply.github.com>
2025-08-31 19:27:14 +0530

    UpdateCors server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(
     origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ['Content-Type', 'token']
   })
 );
 
